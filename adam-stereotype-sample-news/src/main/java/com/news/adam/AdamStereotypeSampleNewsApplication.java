b189adae0909e938c215a3738789aff4b95423c1
anzha12f <anzha12f@gmail.com>
2016-08-26 11:38:43 +1000

    Committing first phase

---
+++ adam-stereotype-sample-news/src/main/java/com/news/adam/AdamStereotypeSampleNewsApplication.java
@@ -0,0 +1,12 @@
+package com.news.adam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdamStereotypeSampleNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdamStereotypeSampleNewsApplication.class, args);
+	}
+}
